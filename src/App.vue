6feabcbf79c9c30755336ae5cd820b69da3e6e6d
Gonzo <gonzalo.araosolguin@gmail.com>
2022-06-27 13:10:12 -0400

    PokéGuía

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,86 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img id="logo" alt="Pokémon logo" src="./assets/logopokemon.png">
+    <h1> PokéGuía</h1>
+    <p>Nombre* o ID (1-898): <input type="text" v-model="nombrepokemon" @keyup.enter="buscarpokemon"> <button v-on:click="buscarpokemon">Buscar</button></p>
+    <h6><i>*Ingresa el nombre en minúsculas y sin tildes</i></h6>
+    <Datos  v-bind:laImagen = "imagenpokemon"
+            v-bind:listaMovimientos="arregloMovimientos"
+            v-bind:listaHabilidades="arregloHabilidades"
+            msg="Datos del Pokémon"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Datos from './components/Datos.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Datos
+  },
+  data(){
+    return{
+      nombrepokemon: 'pikachu',
+      imagenpokemon: '',
+      arregloMovimientos:[],
+      arregloHabilidades:[],
+    }//fin return
+  }, //fin data
+  methods:{
+    buscarpokemon(){
+      this.limpiar();
+      console.log(this.nombrepokemon);
+      let elNombre = this.nombrepokemon;
+      let url = 'https://pokeapi.co/api/v2/pokemon/'+elNombre;
+      console.log(url);
+      fetch(url)
+      .then(response => response.json())
+      .then(json =>{
+        console.log(json);
+        console.log(json.name);
+        console.log(json.moves);
+        console.log(json.abilities);
+        this.imagenpokemon = json.sprites.front_default;
+        for(let unMovimiento of json.moves){
+          this.arregloMovimientos.push(unMovimiento);
+        }
+        for(let unaHabilidad of json.abilities){
+          this.arregloHabilidades.push(unaHabilidad);
+        }
+        console.log("arregloMovimientos =>");
+        console.log(this.arregloMovimientos);
+      });
+    },
+    limpiar(){
+      this.arregloMovimientos = [];
+      this.arregloHabilidades = [];
+    }, // fin limpiar
+  }, // fin methods
+  created(){
+    this.buscarpokemon();
+  }, // fin created
+}// fin export
 </script>
 
 <style>
+body {
+  margin: 0;
+  background-image: url("./assets/pokeball.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #FFF;
   margin-top: 60px;
+}
+#logo{
+  width: 30%;
 }
 </style>
---
+++ src/components/Datos.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <h2>{{ msg }}</h2>
+    <img v-bind:src="laImagen" alt="">
+    <h2>Movimientos</h2>
+    <ul>
+      <li v-for="movimiento in listaMovimientos" :key="movimiento">
+        {{ movimiento.move.name }}
+      </li>
+    </ul>
+    <h2>Habilidades</h2>
+    <ul>
+      <li v-for="habilidad in listaHabilidades" :key="habilidad">
+        {{ habilidad.ability.name }}
+      </li>
+    </ul>
+    <footer>
+      <h6>Gonzalo Araos • Bootcamp Front End 0012, 2022</h6>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Datos',
+  props: {
+    msg: String,
+    laImagen:'',
+    listaMovimientos: [],
+    listaHabilidades:[],
+  },
+  data(){
+    return{
+    otraLista:[],
+    }//Fin return
+  }//Fin data
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+h2 {
+  background-color: rgba(0, 0, 0, 0.7);
+  margin: 50px;
+  border: 1px solid #FFCC00;
+}
+ul {
+  background-color: rgba(0, 0, 0, 0.7);
+  list-style-type: none;
+  padding: 0 20px;
+  margin: 50px;
+  border: 1px solid #FFCC00;
+}
+li {
+  display: inline-block;
+  margin: 5px 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+footer {
+  padding: 10px;
+}
+</style>
